ca1aeeb34a1f56866cce1c471a3cfaed9b2bb478
Yow Cheng Lim <yowcheng.lim@gmail.com>
2020-10-30 16:50:03 +0800

    Initial commit

---
+++ src/main/java/trap/commandcentre/CommandCentreApplication.java
@@ -0,0 +1,13 @@
+package trap.commandcentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandCentreApplication.class, args);
+	}
+
+}
